3f315f3e94da66d9836f7f2e98c8613b33f9d647
변재정 <dogmnil2007@naver.com>
2024-10-17 21:43:11 +0900

    첫번째 커밋

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function Btn(props) {
       style={{ backgroundColor: props.bg_color, color: props.color }}
     >
       <i class="fa-duotone fa-comment"></i> <FontAwesomeIcon icon={faComment} />
-      {props.title}로 로그인
+      {props.title}로 로그인 로그이너라인러미ㅏ
     </button>
   );
 }
